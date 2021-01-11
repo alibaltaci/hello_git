340d84edddb257a944f3a5fa7ac0fc93b2706384
alibaltaci <alibaltaci13@gmail.com>
2021-01-11 15:49:56 +0300

    ozymandias fonksiyonu düzenlendi

--- hi.py
+++ hi.py
@@ -2,4 +2,4 @@ print("hello ai era")
 
 def ozymandias():
     print("My name is Ozymandias, King of king"
-          "Look on my works, e mighy, and despair!")
+          "Look on my works, e mighty, and despair!")
