edcea32318072b2b3a7e9afb458659b2e7b36454
alibaltaci <alibaltaci13@gmail.com>
2021-01-12 13:50:00 +0300

    character_counter fonksiyonu tanımlandı ve içerik oluşturuldu.

---
+++ character_counter.py
@@ -0,0 +1,40 @@
+# Character Counter
+
+# Adımlar:
+
+# 1- Git üzerinden character_counter.py scriptini oluştur, kontrol et ve kaydet.
+# --> type nul> character_counter.py (oluşturuldu)
+# --> git add *
+#--> git commit -m "character_counter.py dosyası oluşturuldu."
+# --> dir (kontrol edildi)
+
+
+
+# 2- Pycharmda character_counter fonksiyonunu oluştur.
+
+def character_counter():
+    txt = input("Enter text")
+    print(txt, "text consists of {} characters".format(len(txt)))
+
+character_counter()
+
+
+
+# 3- git üzerinden değişiklikleri kontrol et.
+# --> git status
+
+
+
+# 4- git üzerinden değişiklikleri kaydet.
+# --> git add *
+
+
+
+# 5- not ekle.
+#--> git commit -m "character_counter fonksiyonu tanımlandı ve içerik oluşturuldu."
+
+
+
+# 6- Değişiklikleri git üzerinden github'da bulunan hello_git reposununa kaydet.
+# --> git push origin master
+
